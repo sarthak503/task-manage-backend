89e1d7d4db0c449e99d15be21eb62749b2d88b1e
sarthak503 <sarthak.kumar503@gmail.com>
2023-06-05 17:26:30 +0530

    added middleware

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const tasksRouter = require('./routes/Task');
 const app = express();
 const port = process.env.PORT||3001; // Change as per your requirements
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(bodyParser.json());
 
 // Connect to MongoDB
